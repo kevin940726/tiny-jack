3126bc5842913c26824a50f3edc1d5d2744240e4
Kai Hao <kai@kaihao.dev>
2020-06-23 20:57:26 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('200');
+});
+
+server.listen(process.env.PORT || 5000);
